74b80af1a3b1bf2540eff03f9c8e7dd557411ea5
a7ridi <a7ridi@github.com>
2020-11-02 01:37:43 +0530

    removed-email-from-settings

--- afre/settings.py
+++ afre/settings.py
@@ -132,13 +132,6 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
 
-# Email Config
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'a7ridi@gmail.com'
-EMAIL_HOST_PASSWORD = 'jzbkjlpkzruekmib'
-EMAIL_USE_TLS = True
-
 try:
     from .local_settings import *
 except ImportError:
